612220461e169d59a3cd903b90f381124baef4d6
AltafAlam Shaikh <gitforaltaf@gmail.com>
2021-04-20 17:01:53 +0530

    init

---
+++ C program/Array/min_max_element_in_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <limits.h>
+
+void minmax(int arr[], int size, int* a, int* b)
+{
+    int min = INT_MAX;
+    int max =0;
+    
+    for(int i=0; i<size; i++)
+    {
+        if(arr[i] < min)
+            min = arr[i];
+        if(arr[i] > max)
+            max = arr[i];
+    }
+    
+    *a = min;
+    *b = max;
+}
+
+int main() {
+	//code
+	int arr[] = { 1000, 11, 445, 10, 330, 3000 };
+	int size = sizeof(arr)/sizeof(arr[0]);
+	
+	int min = 0;
+	int max = 0;
+	
+	minmax(arr, size, &min, &max);
+	
+	printf("%d %d", min, max);
+	
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Data Structures and Algorithms
+This repo consists of some important `Data structure and algoithm` problems and there solutions.
+
+The problems will be based on concepts like: </br>
+<li>Arrays</li>
+<li>Strings</li>
+<li>Searching & Sorting</li>
+<li>Stacks & Queues</li>
+<li>Linked Lists</li>
+<li>Tree</li>
+<li>Graph</li>
+<li>Recursion & Backtracking</li>
+<li>Heap</li>
+<li>Greedy programming</li>
+<li>Dynamic programming</li>
+<li>Bit manipulation, etc</li></br>
+
+This is going to be fun and challenging.
+
+## Let's begin to code.🚀
+
