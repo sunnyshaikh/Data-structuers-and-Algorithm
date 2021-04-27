c57381af5ba5b53ab59c5013e757db6a6af114ef
AltafAlam Shaikh <gitforaltaf@gmail.com>
2021-04-27 20:28:24 +0530

    Minimum & maxinum in array

---
+++ C++ program/Array/min_max_in_array.cpp
@@ -0,0 +1,40 @@
+// C program to find the Minimum and Maximum elemnt from an Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void minmax(int arr[], int size, int *a, int *b)
+{
+    int min = INT_MAX;
+    int max = INT_MIN;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    *a = min;
+    *b = max;
+}
+
+int main()
+{
+    //code
+    int arr[] = {1000, 11, 445, 10, 330, 3000};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int min = 0;
+    int max = 0;
+
+    minmax(arr, size, &min, &max);
+
+    cout << min << " " << max;
+
+    return 0;
+}
+
+// Question from
+// https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
